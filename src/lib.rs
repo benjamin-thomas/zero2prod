650186fe9fa24f0344e768ecfecd0aa37e29ed2e
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-04 17:22:36 +0200

    Add /subscribe POST endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use actix_web::dev::Server;
+use actix_web::web::Form;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
@@ -11,12 +13,24 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().body("UP")
 }
 
+#[derive(Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+// http --form POST localhost:8000/subscribe name=John email=john@example.com
+async fn subscribe(form: Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
+            .route("/subscribe", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- tests/health_test.rs
+++ tests/health_test.rs
@@ -26,3 +26,55 @@ async fn health_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(2), response.content_length()); // "UP"
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let base_url = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=John%20Doe&email=john.doe%40example.com"; // https://www.urlencoder.org
+    let response = client
+        .post(base_url + "/subscribe")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to POST at /subscribe");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16())
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_on_missing_data() {
+    // Arrange
+    let base_url = spawn_app();
+    let endpoint = base_url + "/subscribe";
+    let client = reqwest::Client::new();
+    let cases = vec![
+        ("name=John%20Doe", "missing email"),
+        ("email=john.doe%40example.com", "missing name"),
+        ("", "missing name and email"),
+    ];
+
+    for (body, hint) in cases {
+        // Act
+        let response = client
+            .post(&endpoint)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to POST at /subscribe");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Expected HTTP 400 error (hint: {})",
+            hint,
+        );
+    }
+}
