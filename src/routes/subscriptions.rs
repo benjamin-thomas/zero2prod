93989d23e1efde2be5a39071f0d7837fc7b1d002
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-07 09:40:40 +0200

    Revert getting request ID from app env
    Will enable tracing instead

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,7 @@
 use actix_web::web::Form;
 use actix_web::{web, HttpResponse, Responder};
 use sqlx::PgPool;
-use tracing_actix_web::RequestId;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -11,7 +11,8 @@ pub struct FormData {
 
 // http --form POST localhost:8000/subscribe name=John email=john@example.com
 // while true;do http --form POST localhost:8000/subscribe name=John email=john-$(date +%s)@example.com;sleep 5;done
-pub async fn subscribe(form: Form<FormData>, connection: web::Data<PgPool>, request_id: RequestId) -> impl Responder {
+pub async fn subscribe(form: Form<FormData>, connection: web::Data<PgPool>) -> impl Responder {
+    let request_id = Uuid::new_v4();
     log::info!("[{request_id}] Saving subscriber (name={name}, email={email})", name=form.name, email=form.email, request_id=request_id);
     let res = sqlx::query!(
         r#"
--- src/startup.rs
+++ src/startup.rs
@@ -3,7 +3,6 @@ use actix_web::dev::Server;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
-use tracing_actix_web::TracingLogger;
 use std::net::TcpListener;
 
 
@@ -16,7 +15,6 @@ pub fn run(listener: TcpListener, pg_pool: PgPool) -> Result<Server, std::io::Er
     let server = HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .wrap(TracingLogger::default())
             .route("/health", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
             // Get a pointer copy and attach it to the application state
