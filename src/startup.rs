b067ae8028afcca329e8998a347e06e6adfa3bca
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-25 07:22:35 +0200

    Basic templating with maud

--- src/startup.rs
+++ src/startup.rs
@@ -7,6 +7,25 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
+#[actix_web::get("/hello/{name}")]
+async fn hello1(name: web::Path<String>) -> impl actix_web::Responder {
+    format!("Hello {name}! (1)")
+}
+
+async fn hello2(name: web::Path<String>) -> impl actix_web::Responder {
+    format!("Hello {name}! (2)")
+}
+
+async fn hello_maud(name: web::Path<String>) -> actix_web::Result<maud::Markup> {
+    Ok(maud::html! {
+        html {
+            body {
+                h1 { "Hello " (name.into_inner()) "!" }
+            }
+        }
+    })
+}
+
 pub fn run(
     listener: TcpListener,
     pg_pool: PgPool,
@@ -23,6 +42,10 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
+            .route("/hello2/{name}", web::get().to(hello2))
+            .service(hello1)
+            .route("/hello3/{name}", web::get().to(hello_maud))
+            // .service(hello_maud)
             // Get a pointer copy and attach it to the application state
             .app_data(pg_pool.clone())
             .app_data(pg_queue.clone())
