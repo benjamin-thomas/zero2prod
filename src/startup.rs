eeadcc135dd997984d68e17e7cbc4e7854f061ea
Benjamin <benjamin.guy.thomas@gmail.com>
2022-06-01 07:05:26 +0200

    Set endpoints recap on the front page

--- src/startup.rs
+++ src/startup.rs
@@ -9,18 +9,18 @@ use tracing_actix_web::TracingLogger;
 
 #[actix_web::get("/hello/{name}")]
 async fn hello1(name: web::Path<String>) -> impl actix_web::Responder {
-    format!("Hello {name}! (1)")
+    format!("Hello {name}! (endpoint #1)")
 }
 
 async fn hello2(name: web::Path<String>) -> impl actix_web::Responder {
-    format!("Hello {name}! (2)")
+    format!("Hello {name}! (endpoint #2)")
 }
 
 async fn hello_maud(name: web::Path<String>) -> actix_web::Result<maud::Markup> {
     Ok(maud::html! {
         html {
             body {
-                h1 { "Hello " (name.into_inner()) "!" }
+                h1 { "Hello " (name.into_inner()) "! (endpoint #3)" }
             }
         }
     })
@@ -32,8 +32,11 @@ struct HelloTemplate<'a> {
     name: &'a str,
 }
 
+// I cannot set default values with web::Path<T>
+async fn hello_askama_default() -> impl actix_web::Responder {
+    askama_actix::TemplateToResponse::to_response(&HelloTemplate { name: "world" })
+}
 async fn hello_askama(name: web::Path<String>) -> impl actix_web::Responder {
-    // let name = "world".to_owned();
     askama_actix::TemplateToResponse::to_response(&HelloTemplate { name: &name })
     // HelloTemplate { name: &name }.to_response() // I can get this nicer syntax if I use/import TemplateToResponse
 }
@@ -52,10 +55,20 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            //
+            // Main
+            //
+            .route("/", web::get().to(hello_askama_default))
             .route("/health", web::get().to(health_check))
+            //
+            // Subscribe
+            //
             .route("/subscribe", web::post().to(subscribe))
-            .route("/hello2/{name}", web::get().to(hello2))
+            //
+            // Hello
+            //
             .service(hello1)
+            .route("/hello2/{name}", web::get().to(hello2))
             .route("/hello3/{name}", web::get().to(hello_maud))
             .route("/hello4/{name}", web::get().to(hello_askama))
             // .service(hello_maud)
